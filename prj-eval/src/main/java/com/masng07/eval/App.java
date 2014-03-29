fa00b58fe50eff59dd84fae9a44baa18088e1167
northlaker <masng07@gmail.com>
2014-03-29 08:24:09 -0700

    created eval prj.

---
+++ prj-eval/src/main/java/com/masng07/eval/App.java
@@ -0,0 +1,13 @@
+package com.masng07.eval;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
